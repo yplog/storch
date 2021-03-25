69e5886155965651ef0e38a52b8106d2ad4e4730
yp <yalinpala@protonmail.com>
2021-03-25 16:37:17 +0300

    require

--- main.lua
+++ main.lua
@@ -1,8 +1,21 @@
+require('src.menus.deneme')
+
 function love.load()
+  gameState = 0
 end
 
 function love.update(dt)
+  if love.keyboard.isDown("return") then
+    gameState = 1
+  elseif love.keyboard.isDown("escape") then
+    gameState = 0
+  end
 end
 
 function love.draw()
+  if gameState == 0 then
+    love.graphics.print(startMenu)
+  elseif gameState == 1 then
+    love.graphics.print("Game Screen")
+  end
 end
