2728037d7dc6ad353f4ba2df605df3993abf9cd1
yp <yalinpala@protonmail.com>
2021-03-26 13:15:57 +0300

    exit, options game screen grid

--- main.lua
+++ main.lua
@@ -1,13 +1,33 @@
-require('src.menus.deneme')
+require('src.menus.start')
 
 function love.load()
   gameState = 0
 end
 
 function love.update(dt)
-  if love.keyboard.isDown("return") then
-    gameState = 1
-  elseif love.keyboard.isDown("escape") then
+  x, y = love.mouse.getPosition()
+  
+  if gameState == 0 then
+    if (x >= 100 and x <= 200) and (y >= 500 and y <= 530) then
+      if love.mouse.isDown(1) then
+        gameState = 1
+      end	
+    end
+  
+    if (x >= 250 and x <= 350) and (y >= 500 and y <= 530) then
+      if love.mouse.isDown(1) then
+        gameState = 2
+      end	
+    end
+    
+    if (x >= 400 and x <= 500) and (y >= 500 and y <= 530) then
+      if love.mouse.isDown(1) then
+        love.event.quit()
+      end	
+    end
+  end
+  
+  if love.keyboard.isDown("escape") then
     gameState = 0
   end
 end
@@ -15,7 +35,22 @@ end
 function love.draw()
   if gameState == 0 then
     love.graphics.print(startMenu)
+    startButton()
+    optionButton()
+    exitButton()
   elseif gameState == 1 then
     love.graphics.print("Game Screen")
+    local y = 0
+    for y = 0, 550 do 
+      if 0 == (y % 50) then
+        for x = 0, 750 do
+          if 0 == (x % 50) then
+            love.graphics.rectangle('line', x, y, 50, 50)
+          end
+        end
+      end
+    end
+  elseif gameState == 2 then
+    love.graphics.print("Options Screen")
   end
 end
--- src/menus/start.lua
+++ src/menus/start.lua
@@ -1,1 +1,16 @@
 startMenu = "Start Screen"
+
+function startButton()
+  love.graphics.rectangle('fill', 100, 500, 100, 30)
+  love.graphics.print({{0, 0, 0}, 'Start'}, 130, 509)
+end
+
+function optionButton() 
+  love.graphics.rectangle('fill', 250, 500, 100, 30)
+  love.graphics.print({{0, 0, 0}, 'Options'}, 280, 509)
+end
+
+function exitButton() 
+  love.graphics.rectangle('fill', 400, 500, 100, 30)
+  love.graphics.print({{0, 0, 0}, 'Exit'}, 420, 509)
+end
